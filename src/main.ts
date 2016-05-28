fda8ef2132b119c3cde80a6efe080ad1ea151b50
saulocamposi <saulocamposi@gmail.com>
2016-05-28 19:18:23 -0300

    Added routers providers

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { SampleCliAngular2AppComponent, environment } from './app/';
+import { ROUTER_PROVIDERS } from '@angular/router-deprecated'
+
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(SampleCliAngular2AppComponent);
+bootstrap(SampleCliAngular2AppComponent,[ROUTER_PROVIDERS]);
