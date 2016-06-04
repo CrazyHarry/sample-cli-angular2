99698f0b558fc394036662f92d2264393183cbce
saulocamposi <saulocamposi@gmail.com>
2016-06-03 21:22:09 -0300

    Updated inserted directives

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+//import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
 
 @Component({
   moduleId: module.id,
   selector: 'app-first',
   templateUrl: 'first.component.html',
-  styleUrls: ['first.component.css']
-})
+  styleUrls: ['first.component.css'],
+  directives: [MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES]
+  //providers: [MdIconRegistry]
+  })
 export class FirstComponent implements OnInit {
 
   constructor() {}
