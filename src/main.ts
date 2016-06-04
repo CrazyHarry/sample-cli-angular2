e59afc339b8ebbd0530c9adff8f0d15b396cd4ca
saulocamposi <saulocamposi@gmail.com>
2016-06-03 21:07:16 -0300

    Updated http_providers

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { SampleCliAngular2AppComponent, environment } from './app/';
 import { ROUTER_PROVIDERS } from '@angular/router-deprecated'
-
+import { HTTP_PROVIDERS } from '@angular/http';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(SampleCliAngular2AppComponent,[ROUTER_PROVIDERS]);
+bootstrap(SampleCliAngular2AppComponent,[ROUTER_PROVIDERS,HTTP_PROVIDERS]);
